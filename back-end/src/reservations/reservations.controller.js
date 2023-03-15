6f82b07ba68800f4b5b3ad8382d53d9625fffeb4
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-15 15:02:47 -0500

    GET/reservations needed a list function that would list all of the reservations if the date query does not exist

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -9,11 +9,11 @@ async function list(req, res) {
   //  if the URL is /dashboard?date=2035-12-30 then send a GET to /reservations?date=2035-12-30 to list the reservations for that date)
   // date defaulted to today
   if (date) {
-  // console.log("line 13**************************", date);
     const data = await service.listAllReservationsForOneDate(date);
-    console.log("line 14************************", data)
-    res.json({ data });
+    return res.json({ data });
   }
+  const data = await service.list();
+  return res.json({ data });
 }
 
 module.exports = {
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,13 +1,18 @@
 const knex = require("../db/connection");
 
+function list() {
+  return knex("reservations");
+}
+
 // list all reservations for one date only
 // reservations sorted by time
 function listAllReservationsForOneDate(date) {
   return knex("reservations")
     .where("reservation_date", date)
-    .orderBy("reservation_time")
+    .orderBy("reservation_time");
 }
 
 module.exports = {
+  list,
   listAllReservationsForOneDate,
 };
