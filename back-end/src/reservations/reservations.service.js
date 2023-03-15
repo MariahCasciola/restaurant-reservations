bc00e47d6df34df7b972e325bdb59c1aa74129a1
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-15 13:31:07 -0500

    GET /reservations route is functional and passes tests

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -3,11 +3,9 @@ const knex = require("../db/connection");
 // list all reservations for one date only
 // reservations sorted by time
 function listAllReservationsForOneDate(date) {
-    console.log("inside list")
   return knex("reservations")
     .where("reservation_date", date)
     .orderBy("reservation_time")
-    // .then((data)=>{console.log("list********", data)})
 }
 
 module.exports = {
