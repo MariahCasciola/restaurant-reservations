63d1b361431f0abc60033d1db2d1cd194b11fc7c
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-16 16:39:49 -0500

    reservation form saves dates on submit, and submit button redirects us to the home page with new reservation displaying

---
+++ front-end/src/ReservationsForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { createReservations } from "./utils/api";
+
+function ReservationsForm() {
+  const history = useHistory();
+
+  const initialFormState = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  };
+  const [formData, setFormData] = useState({ ...initialFormState });
+
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // call a create function from api.js
+    // call with {..reservation, people: Number(reservation.people)}
+    async function createNewReservation() {
+      try {
+        const newReservation = await createReservations({
+          ...formData,
+          people: Number(formData.people),
+        });
+        setFormData({ ...initialFormState });
+        history.push(`/dashboard?date=${newReservation.reservation_date}`);
+      } catch (error) {}
+    }
+    createNewReservation();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first_name">
+          First Name:
+          <input
+            name="first_name"
+            type="text"
+            onChange={handleChange}
+            value={formData.first_name}
+          />
+        </label>
+        <label htmlFor="last_name">
+          Last Name:
+          <input
+            name="last_name"
+            type="text"
+            onChange={handleChange}
+            value={formData.last_name}
+          />
+        </label>
+        <label htmlFor="mobile_number">
+          Mobile number:
+          <input
+            name="mobile_number"
+            type="text"
+            onChange={handleChange}
+            value={formData.mobile_number}
+          />
+        </label>
+        <label htmlFor="reservation_date">
+          Date of reservation :
+          <input
+            name="reservation_date"
+            type="date"
+            onChange={handleChange}
+            value={formData.reservation_date}
+          />
+        </label>
+        <label htmlFor="reservation_time">
+          Time of reservation:
+          <input
+            name="reservation_time"
+            type="time"
+            onChange={handleChange}
+            value={formData.reservation_time}
+          />
+        </label>
+        <label htmlFor="people">
+          Number of People:
+          <input
+            name="people"
+            type="number"
+            onChange={handleChange}
+            value={formData.people}
+          />
+        </label>
+        <button type="submit">Submit</button>
+        {/* type reset, or type cancel? */}
+        <button type="button" onClick={() => history.goBack()}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ReservationsForm;
