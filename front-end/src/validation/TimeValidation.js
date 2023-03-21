b8b241f481b64e263507063cfb1858f92a78b519
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-20 22:00:43 -0500

    displays an error message if reservation time is before or equal to 10:30 AM, displays an error message if reservation time is eqaul or after 9:30 pm an hour before closing

---
+++ front-end/src/validation/TimeValidation.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function TimeValidation({ errors }) {
+  if (errors) {
+    // checking for tuesday
+    if (errors.message === "Reservation time must be at 10:30 am, or later.") {
+      return (
+        <div className="alert alert-danger">
+          Time of reservation must be made between 10:30 am and 9:30 pm.
+        </div>
+      );
+    }
+    // checking for days
+    else if (
+      errors.message ===
+      "Reservation time must be at 9:30 pm, or earlier, the restaurant will be closed at 10:30 pm."
+    ) {
+      return (
+        <div className="alert alert-danger">
+          Time of reservation must be made between 10:30 am and 9:30 pm, the
+          restaurant closes at 10:30 pm.
+        </div>
+      );
+    }
+  }
+  return null;
+}
+
+export default TimeValidation;
