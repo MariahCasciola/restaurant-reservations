f705a2f925ebd70dcbccea5d94b56d361a9a1f7f
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-22 13:58:46 -0500

    updated packaged-lock.json, POST /tables returns 201 status when a table is created, and validation returns 400 and the proper string messages when the table being created does not meet requirements

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -12,7 +12,10 @@ async function tableExists(req, res, next) {
     table = res.locals.table;
     return next();
   }
-  next({ status: 404, message: `Table id ${req.params.table_id} does not exist` });
+  next({
+    status: 404,
+    message: `Table id ${req.params.table_id} does not exist`,
+  });
 }
 
 function read(req, res, next) {
@@ -21,8 +24,49 @@ function read(req, res, next) {
   res.json({ data });
 }
 
+const validProperties = ["table_name", "capacity"];
+
+function hasProperties(...properties) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+
+    try {
+      properties.forEach((property) => {
+        if (!data[property]) {
+          const error = new Error(`A '${property}' property is required.`);
+          error.status = 400;
+          throw error;
+        }
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+function tableCharacterLengthValidator(req, res, next) {
+  const tableName = req.body.data.table_name;
+  if (tableName.length === 1) {
+    next({
+      status: 400,
+      message: "table_name must be longer than one character.",
+    });
+  }
+  return next();
+}
+
+function capacityIsANumberValidator(req, res, next) {
+  const tableCapacity = req.body.data.capacity;
+  if (!Number.isInteger(tableCapacity) || tableCapacity === 0) {
+    next({ status: 400, message: "capacity must be a number" });
+  }
+  return next();
+}
+
 async function create(req, res, next) {
-  return next({ status: 500, message: "Get fucked idiot" });
+  const data = await service.create(req.body.data);
+  res.status(201).json({ data });
 }
 
 // put request to update, needs an table_id and a body
@@ -41,6 +85,12 @@ async function update(req, res, next) {
 module.exports = {
   list: [asyncErrorBoundary(list)],
   read: [asyncErrorBoundary(tableExists), read],
-  create: [asyncErrorBoundary(create)],
+  create: [
+    hasProperties("table_name"),
+    hasProperties("capacity"),
+    tableCharacterLengthValidator,
+    capacityIsANumberValidator,
+    asyncErrorBoundary(create),
+  ],
   update: [asyncErrorBoundary(update)],
 };
